e8ec2c05c53299658dff271334a9f900c2cfc3b5
pr0crustes <dark.pr0crustes@gmail.com>
2019-02-06 13:25:05 -0200

    Nyx rework.

---
+++ game/scripts/vscripts/abilities/heroes/nyx_assassin_custom_vendetta.lua
@@ -0,0 +1,91 @@
+
+
+LinkLuaModifier("modifier_nyx_assassin_custom_vendetta", "abilities/heroes/nyx_assassin_custom_vendetta.lua", LUA_MODIFIER_MOTION_NONE)
+
+
+modifier_nyx_assassin_custom_vendetta = class({})
+
+
+function modifier_nyx_assassin_custom_vendetta:GetTexture()
+    return "nyx_assassin_vendetta"
+end
+
+
+function modifier_nyx_assassin_custom_vendetta:CheckState()
+    return {
+        [MODIFIER_STATE_INVISIBLE] = true,
+        [MODIFIER_STATE_NO_UNIT_COLLISION] = true,
+        [MODIFIER_STATE_CANNOT_MISS] = true,
+    }
+end
+
+
+function modifier_nyx_assassin_custom_vendetta:OnCreated()
+    local ability = self:GetAbility()
+
+    if not ability then
+        self:Destroy()
+        return nil
+    end
+
+    self.crit_increase = ability:GetSpecialValueFor("crit_increase")
+    self.current_crit = self.crit_increase
+
+    self.interval = ability:GetSpecialValueFor("interval")
+
+    self:StartIntervalThink(self.interval)
+end
+
+
+function modifier_nyx_assassin_custom_vendetta:OnIntervalThink()
+    self.current_crit = self.current_crit + self.crit_increase
+end
+
+
+function modifier_nyx_assassin_custom_vendetta:DeclareFunctions()
+    return {
+        MODIFIER_PROPERTY_INVISIBILITY_LEVEL,
+        MODIFIER_PROPERTY_PREATTACK_CRITICALSTRIKE,
+        MODIFIER_EVENT_ON_ATTACK_LANDED,
+        MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE,
+    }
+end
+
+
+function modifier_nyx_assassin_custom_vendetta:GetModifierInvisibilityLevel()
+    return 1
+end
+
+
+function modifier_nyx_assassin_custom_vendetta:GetModifierPreAttack_CriticalStrike()
+    return self.current_crit
+end
+
+
+function modifier_nyx_assassin_custom_vendetta:OnAttackLanded(keys)
+    local attacker = keys.attacker
+    if attacker == self:GetParent() then 
+        self:Destroy() 
+    end
+end
+
+
+function modifier_nyx_assassin_custom_vendetta:GetModifierMoveSpeedBonus_Percentage()
+    return self:GetAbility():GetSpecialValueFor("movement_speed")
+end
+
+
+function cast_nyx_assassin_custom_vendetta(keys)
+    local caster = keys.caster
+    local ability = keys.ability
+    
+    local duration = ability:GetSpecialValueFor("duration")
+
+    local talent = caster:FindAbilityByName("nyx_assassin_custom_bonus_unique_1")
+    if talent and talent:GetLevel() > 0 then
+        duration = duration + talent:GetSpecialValueFor("value")
+    end
+
+    caster:AddNewModifier(caster, ability, "modifier_nyx_assassin_custom_vendetta", {duration = duration})
+end
+
