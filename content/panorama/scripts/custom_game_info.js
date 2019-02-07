3216bb8b6070ee6fac30fae98c2081b783842c40
pr0crustes <dark.pr0crustes@gmail.com>
2019-02-07 13:17:53 -0200

    Started working on an info menu.

---
+++ content/panorama/scripts/custom_game_info.js
@@ -0,0 +1,18 @@
+
+
+var open = false;
+
+
+function get_hud() {
+    var p = $.GetContextPanel();
+    while (p !== null && p.id !== "Hud") {
+        p = p.GetParent();
+    }
+    return p;
+}
+
+
+function on_info_button_click() {
+    get_hud().FindChildTraverse("game_info_button").GetParent().style.transform = (open ? "translateX(-450px)" : "translateX(0)");
+    open = !open;
+}
