3c9778e4d2a782523039ce59850c60da850fdb2d
pr0crustes <dark.pr0crustes@gmail.com>
2019-02-13 07:24:38 -0200

    Fixed triumviratus stacks and added a fx.

--- game/scripts/vscripts/items/triumviratus.lua
+++ game/scripts/vscripts/items/triumviratus.lua
@@ -3,16 +3,55 @@ require("abilities/other/modify_hud_modifier")
 
 
 
+local function new_stack_count(caster, modifier, max)
+    local stack_count = 0
+    if caster:HasModifier(modifier) then
+        stack_count = caster:GetModifierStackCount(modifier, caster)
+    end
+
+    local new_stack = stack_count + 1
+    
+    if new_stack > max then  -- clamp at max.
+        new_stack = max
+    end
+    return new_stack
+end
+
+
+
+function on_attack_landed(keys)
+    local caster = keys.caster
+    local ability = keys.ability
+    local modifier = keys.modifier
+    local max = keys.max
+    local duration = keys.duration
+
+    local stack_count = new_stack_count(caster, modifier, max)
+
+    ability:ApplyDataDrivenModifier(caster, caster, modifier, {duration = duration})
+    caster:SetModifierStackCount(modifier, caster, stack_count)
+end
+
+
+
 -- Items, toggles and autocast should not trigger Triumviratus.
 function on_ability_executed(keys)
     local caster = keys.caster
+    local ability = keys.ability
+    local modifier = keys.modifier
+    local max = keys.max
     local used_ability = keys.event_ability
+    local duration = keys.duration
 
 
     if used_ability 
         and not used_ability:IsItem()
         and not ability_behavior_includes(used_ability, DOTA_ABILITY_BEHAVIOR_TOGGLE)
         and not ability_behavior_includes(used_ability, DOTA_ABILITY_BEHAVIOR_AUTOCAST) then
-        add_modifier_if_not_max(keys)
+
+        local stack_count = new_stack_count(caster, modifier, max)
+    
+        ability:ApplyDataDrivenModifier(caster, caster, modifier, {duration = duration})
+        caster:SetModifierStackCount(modifier, caster, stack_count)
     end
 end
