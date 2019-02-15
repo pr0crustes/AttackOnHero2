425f5aeaee350eff716214ef8d56e3f913b75974
pr0crustes <dark.pr0crustes@gmail.com>
2019-02-15 07:43:11 -0200

    Refactored precaching.

--- game/scripts/vscripts/addon_game_mode.lua
+++ game/scripts/vscripts/addon_game_mode.lua
@@ -3,9 +3,67 @@ require("statcollection/init")
 
 
 
+function do_precache(elements, handle)
+	for _, e in ipairs(elements) do
+		handle(e)
+	end
+end
+
+
 function Precache(context)
-	PrecacheItemByNameSync("item_tombstone", context)
-	PrecacheItemByNameSync("item_bag_of_gold", context)
+
+	local items = {
+		"item_bag_of_gold",
+		"item_tombstone",
+	}
+
+	local models = {
+
+	}
+
+	local particles = {
+
+	}
+
+	local soundevents = {
+
+	}
+
+	local units = { 
+
+	}
+
+
+
+	do_precache(items, 
+		function(e) 
+			PrecacheItemByNameSync(e, context) 
+		end
+	)
+
+	do_precache(models, 
+		function(e) 
+			PrecacheModel(e, context)
+		end
+	)
+
+	do_precache(particles, 
+		function(e) 
+			PrecacheResource("particle", e, context)
+		end
+	)
+
+	do_precache(soundevents, 
+		function(e) 
+			PrecacheResource("soundfile", e, context)
+		end
+	)
+
+	do_precache(units, 
+		function(e) 
+			PrecacheUnitByNameSync(e, context)
+		end
+	)
 end
 
 
