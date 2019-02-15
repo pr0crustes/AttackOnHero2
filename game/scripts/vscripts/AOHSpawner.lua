70b188df0bb25afc41f1d1b52a5d6836b3f24ad5
pr0crustes <dark.pr0crustes@gmail.com>
2019-02-15 07:23:01 -0200

    Refactored Spawner.

--- game/scripts/vscripts/AOHSpawner.lua
+++ game/scripts/vscripts/AOHSpawner.lua
@@ -10,40 +10,31 @@ function AOHSpawner:ReadConfiguration(name, kv, gameRound)
 	self._gameRound = gameRound
 	self._dependentSpawners = {}
 
-	self._szChampionNPCClassName = kv.ChampionNPCName or ""
 	self._szGroupWithUnit = kv.GroupWithUnit or ""
 	self._szName = name
 	self._szNPCClassName = kv.NPCName or ""
-	self._szSpawnerName = kv.SpawnerName or ""
 	self._szWaitForUnit = kv.WaitForUnit or ""
 	self._szWaypointName = kv.Waypoint or ""
 	self._waypointEntity = nil
 
-	self._nChampionLevel = tonumber(kv.ChampionLevel or 1)
-	self._nChampionMax = tonumber(kv.ChampionMax or 1)
-	self._nCreatureLevel = tonumber(kv.CreatureLevel or 1)
 	self._nTotalUnitsToSpawn = tonumber(kv.TotalUnitsToSpawn or 0)
 	self._nUnitsPerSpawn = tonumber(kv.UnitsPerSpawn or 0)
 	self._nUnitsPerSpawn = tonumber(kv.UnitsPerSpawn or 1)
 
-	self._flChampionChance = tonumber(kv.ChampionChance or 0)
 	self._flInitialWait = tonumber(kv.WaitForTime or 0)
 	self._flSpawnInterval = tonumber(kv.SpawnInterval or 0)
-
-	self._bDontGiveGoal = (tonumber(kv.DontGiveGoal or 0) ~= 0)
-	self._bDontOffsetSpawn = (tonumber(kv.DontOffsetSpawn or 0) ~= 0)
 end
 
 
 function AOHSpawner:PostLoad(spawnerList)
-	self._waitForUnit = spawnerList[ self._szWaitForUnit ]
+	self._waitForUnit = spawnerList[self._szWaitForUnit]
 	if self._szWaitForUnit ~= "" and not self._waitForUnit then
 		debug_print(self._szName .. " has a wait for unit " .. self._szWaitForUnit .. " that is missing from the round data.")
 	elseif self._waitForUnit then
 		table.insert(self._waitForUnit._dependentSpawners, self)
 	end
 
-	self._groupWithUnit = spawnerList[ self._szGroupWithUnit ]
+	self._groupWithUnit = spawnerList[self._szGroupWithUnit]
 	if self._szGroupWithUnit ~= "" and not self._groupWithUnit then
 		debug_print (self._szName .. " has a group with unit " .. self._szGroupWithUnit .. " that is missing from the round data.")
 	elseif self._groupWithUnit then
@@ -54,9 +45,6 @@ end
 
 function AOHSpawner:Precache()
 	PrecacheUnitByNameAsync(self._szNPCClassName, function(sg) self._sg = sg end)
-	if self._szChampionNPCClassName ~= "" then
-		PrecacheUnitByNameAsync(self._szChampionNPCClassName, function(sg) self._sgChampion = sg end)
-	end
 end
 
 
@@ -66,15 +54,9 @@ function AOHSpawner:Begin()
 	self._nUnitsCurrentlyAlive = 0
 	
 	self._vecSpawnLocation = nil
-	if self._szSpawnerName ~= "" then
-		local entSpawner = Entities:FindByName(nil, self._szSpawnerName)
-		if not entSpawner then
-			debug_print(string.format("Failed to find spawner named %s for %s\n", self._szSpawnerName, self._szName))
-		end
-		self._vecSpawnLocation = entSpawner:GetAbsOrigin()
-	end
+
 	self._entWaypoint = nil
-	if self._szWaypointName ~= "" and not self._bDontGiveGoal then
+	if self._szWaypointName ~= "" then
 		self._entWaypoint = Entities:FindByName(nil, self._szWaypointName)
 		if not self._entWaypoint then
 			debug_print(string.format("Failed to find waypoint named %s for %s", self._szWaypointName, self._szName))
@@ -178,12 +160,11 @@ function AOHSpawner:_UpdateRandomSpawn()
 	end
 	self._vecSpawnLocation = entSpawner:GetAbsOrigin()
 
-	if not self._bDontGiveGoal then
-		self._entWaypoint = Entities:FindByName(nil, spawnInfo.szFirstWaypoint)
-		if not self._entWaypoint then
-			debug_print(string.format("Failed to find a waypoint named %s for %s.", spawnInfo.szFirstWaypoint, self._szName))
-			return
-		end
+
+	self._entWaypoint = Entities:FindByName(nil, spawnInfo.szFirstWaypoint)
+	if not self._entWaypoint then
+		debug_print(string.format("Failed to find a waypoint named %s for %s.", spawnInfo.szFirstWaypoint, self._szName))
+		return
 	end
 end
 
@@ -197,9 +178,9 @@ function AOHSpawner:_DoSpawn()
 		debug_print(string.format("Started spawning %s at %.2f", self._szName, GameRules:GetGameTime()))
 	end
 
-	if self._szSpawnerName == "" then
-		self:_UpdateRandomSpawn()
-	end
+
+	self:_UpdateRandomSpawn()
+
 
 	local vBaseSpawnLocation = self:_GetSpawnLocation()
 	if not vBaseSpawnLocation then 
@@ -207,36 +188,10 @@ function AOHSpawner:_DoSpawn()
 	end
 	for iUnit = 1,nUnitsToSpawn do
 
-		local bIsChampion = RollPercentage(self._flChampionChance)
-		if self._nChampionsSpawnedThisRound >= self._nChampionMax then
-			bIsChampion = false
-		end
+		local vSpawnLocation = vBaseSpawnLocation + RandomVector(RandomFloat(0, 200))
 
-
-		local szNPCClassToSpawn = self._szNPCClassName
-		if bIsChampion and self._szChampionNPCClassName ~= "" then
-			szNPCClassToSpawn = self._szChampionNPCClassName
-		end
-
-		local vSpawnLocation = vBaseSpawnLocation
-		if not self._bDontOffsetSpawn then
-			vSpawnLocation = vSpawnLocation + RandomVector(RandomFloat(0, 200))
-		end
-
-		local entUnit = CreateUnitByName(szNPCClassToSpawn, vSpawnLocation, true, nil, nil, DOTA_TEAM_BADGUYS)
+		local entUnit = CreateUnitByName(self._szNPCClassName, vSpawnLocation, true, nil, nil, DOTA_TEAM_BADGUYS)
 		if entUnit then
-			if entUnit:IsCreature() then
-				if bIsChampion then
-					self._nChampionsSpawnedThisRound = self._nChampionsSpawnedThisRound + 1
-					entUnit:CreatureLevelUp((self._nChampionLevel - 1))
-					entUnit:SetChampion(true)
-					local nParticle = ParticleManager:CreateParticle("heavens_halberd", PATTACH_ABSORIGIN_FOLLOW, entUnit)
-					ParticleManager:ReleaseParticleIndex(nParticle)
-					entUnit:SetModelScale(1.1, 0)
-				else
-					entUnit:CreatureLevelUp(self._nCreatureLevel - 1)
-				end
-			end
 			local entWp = self:_GetSpawnWaypoint()
 			if entWp ~= nil then
 				entUnit:SetInitialGoalEntity(entWp)
