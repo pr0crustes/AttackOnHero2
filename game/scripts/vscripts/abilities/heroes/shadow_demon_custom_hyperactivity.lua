42fca7da76e4bf0ecb345e72b53588606a7821d0
pr0crustes <dark.pr0crustes@gmail.com>
2019-02-17 06:25:48 -0300

    Optimizing Shadow Demon ultimate.

--- game/scripts/vscripts/abilities/heroes/shadow_demon_custom_hyperactivity.lua
+++ game/scripts/vscripts/abilities/heroes/shadow_demon_custom_hyperactivity.lua
@@ -2,25 +2,36 @@ require("lib/timers")
 
 
 
+local exclude_items = {
+    item_arcane_boots = true,
+    item_custom_refresher = true,
+    item_guardian_greaves = true,
+    item_sheepstick = true,
+}
+
+
+
 function on_ability_executed(keys)
     local caster = keys.caster
     local ability = keys.ability
     local used_ability = keys.event_ability
 
-    print("aaaaaaaaaa")
-
     if used_ability 
         and used_ability:GetCaster() == caster 
         and used_ability ~= ability 
-        and used_ability:GetName() ~= "item_custom_refresher" then
+        and not exclude_items[used_ability:GetAbilityName()] then
 
-        print("bbbbbbbb")
+        local cooldown = ability:GetSpecialValueFor("cooldown")
+
         Timers:CreateTimer(
-            0.2, 
             function()
                 if used_ability then
-                    used_ability:EndCooldown()
-                    used_ability:StartCooldown(1.0)
+                    if used_ability:GetCooldownTimeRemaining() > 0 then
+                        used_ability:EndCooldown()
+                        used_ability:StartCooldown(cooldown)
+                        return nil
+                    end
+                    return 0.05
                 end
             end
         )
