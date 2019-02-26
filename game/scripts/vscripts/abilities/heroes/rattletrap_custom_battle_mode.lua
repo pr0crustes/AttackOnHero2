d3998ca86dee55d3a6e35f78aaaea491333a59b3
pr0crustes <dark.pr0crustes@gmail.com>
2019-02-26 13:33:02 -0300

    Added Clockwerk Battle mode talents.

--- game/scripts/vscripts/abilities/heroes/rattletrap_custom_battle_mode.lua
+++ game/scripts/vscripts/abilities/heroes/rattletrap_custom_battle_mode.lua
@@ -4,14 +4,26 @@
 rattletrap_custom_battle_mode = class({})
 
 
-function rattletrap_custom_battle_mode:OnToggle()
-    local caster = self:GetCaster()
-    caster:EmitSound("Hero_Rattletrap.Power_Cogs")
+if IsServer() then
+    function rattletrap_custom_battle_mode:OnToggle()
+        local caster = self:GetCaster()
+        caster:EmitSound("Hero_Rattletrap.Power_Cogs")
 
-    if self:GetToggleState() then
-        caster:AddNewModifier(caster, self, "modifier_rattletrap_custom_battle_mode", {})
-    else
-        caster:RemoveModifierByName("modifier_rattletrap_custom_battle_mode")
+        if self:GetToggleState() then
+            caster:AddNewModifier(caster, self, "modifier_rattletrap_custom_battle_mode", {})
+
+            if talent_value(caster, "rattletrap_custom_bonus_unique_3") > 0 then
+                caster:AddNewModifier(caster, self, "modifier_rattletrap_custom_battle_mode_talent_armor", {})
+            end
+
+            if talent_value(caster, "rattletrap_custom_bonus_unique_4") > 0 then
+                caster:AddNewModifier(caster, self, "modifier_rattletrap_custom_battle_mode_talent_magic_res", {})
+            end
+        else
+            caster:RemoveModifierByName("modifier_rattletrap_custom_battle_mode")
+            caster:RemoveModifierByName("modifier_rattletrap_custom_battle_mode_talent_armor")
+            caster:RemoveModifierByName("modifier_rattletrap_custom_battle_mode_talent_magic_res")
+        end
     end
 end
 
@@ -49,4 +61,40 @@ end
 
 function modifier_rattletrap_custom_battle_mode:GetModifierModelScale()
     return 50
+end
+
+
+
+LinkLuaModifier("modifier_rattletrap_custom_battle_mode_talent_armor", "abilities/heroes/rattletrap_custom_battle_mode.lua", LUA_MODIFIER_MOTION_NONE)
+
+modifier_rattletrap_custom_battle_mode_talent_armor = class({})
+
+
+function modifier_rattletrap_custom_battle_mode_talent_armor:DeclareFunctions()
+    return {
+        MODIFIER_PROPERTY_PHYSICAL_ARMOR_BONUS,
+    }
+end
+
+
+function modifier_rattletrap_custom_battle_mode_talent_armor:GetModifierPhysicalArmorBonus()
+    return self:GetAbility():GetSpecialValueFor("talent_armor")
+end
+
+
+
+LinkLuaModifier("modifier_rattletrap_custom_battle_mode_talent_magic_res", "abilities/heroes/rattletrap_custom_battle_mode.lua", LUA_MODIFIER_MOTION_NONE)
+
+modifier_rattletrap_custom_battle_mode_talent_magic_res = class({})
+
+
+function modifier_rattletrap_custom_battle_mode_talent_magic_res:DeclareFunctions()
+    return {
+        MODIFIER_PROPERTY_MAGICAL_RESISTANCE_BONUS,
+    }
+end
+
+
+function modifier_rattletrap_custom_battle_mode_talent_magic_res:GetModifierMagicalResistanceBonus()
+    return self:GetAbility():GetSpecialValueFor("talent_magic_res")
 end
