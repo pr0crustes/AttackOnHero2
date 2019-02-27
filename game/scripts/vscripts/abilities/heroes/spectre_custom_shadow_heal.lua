bdc072a5e41bef08dc79edf092ceb25559c10fba
pr0crustes <dark.pr0crustes@gmail.com>
2019-02-27 16:35:29 -0300

    Spectre Shadow Heal: No longer disables health regen.

--- game/scripts/vscripts/abilities/heroes/spectre_custom_shadow_heal.lua
+++ game/scripts/vscripts/abilities/heroes/spectre_custom_shadow_heal.lua
@@ -1,17 +1,11 @@
-require("lib/my")
-
 
 
 function cast_shadow_heal(keys)
     local caster = keys.caster
     local ability = keys.ability
 
-    local duration = ability:GetSpecialValueFor("duration")
-
-    local heal = caster:GetHealthRegen() * duration
+    local heal = caster:GetHealthRegen() * ability:GetSpecialValueFor("multiplier")
 
     caster:Heal(heal, ability)
-
-    ability:ApplyDataDrivenModifier(caster, caster, "modifier_custom_shadow_heal_negation", {duration = duration})
 end
 
