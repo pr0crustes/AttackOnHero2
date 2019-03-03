e6bc90a8d5a1445df18b476204c5b6c82a40f81c
pr0crustes <dark.pr0crustes@gmail.com>
2019-03-03 09:36:13 -0300

    Starting to work on hellbear.

--- game/scripts/vscripts/AOHGameMode.lua
+++ game/scripts/vscripts/AOHGameMode.lua
@@ -30,7 +30,7 @@ end
 
 
 function AOHGameMode:InitGameMode()
-	self._nRoundNumber = 1
+	self._nRoundNumber = 35
 	self._currentRound = nil
 	self._entAncient = Entities:FindByName(nil, "dota_goodguys_fort")
 
