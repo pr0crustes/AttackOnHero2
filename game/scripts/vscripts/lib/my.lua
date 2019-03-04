320b69767b12097413cde2b7b9ce6a114244b0d1
pr0crustes <dark.pr0crustes@gmail.com>
2019-03-04 07:47:19 -0300

    Added cold ground to lich boss.

--- game/scripts/vscripts/lib/my.lua
+++ game/scripts/vscripts/lib/my.lua
@@ -247,3 +247,8 @@ function kill_dummy(dummy)
 		end
 	)
 end
+
+
+function create_dummy(caster, pos)
+	return CreateUnitByName("npc_dummy_unit", pos, false, caster, caster, caster:GetTeamNumber())
+end
