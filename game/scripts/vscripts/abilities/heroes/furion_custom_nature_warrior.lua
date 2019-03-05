c95ad69533655db56680907eee3095ec6e045ee9
pr0crustes <dark.pr0crustes@gmail.com>
2019-03-05 17:03:36 -0300

    First Furion Nature Warrior version.

---
+++ game/scripts/vscripts/abilities/heroes/furion_custom_nature_warrior.lua
@@ -0,0 +1,53 @@
+require("lib/illusion")
+
+
+furion_custom_nature_warrior = class({})
+
+
+if IsServer() then
+    function furion_custom_nature_warrior:OnSpellStart()
+        self.target_pos = self:GetCursorPosition()
+
+        self.duration = self:GetSpecialValueFor("duration")
+        self.health_multiplier = self:GetSpecialValueFor("health_multiplier")
+        self.armor_multiplier = self:GetSpecialValueFor("armor_multiplier")
+        self.magic_resistance = self:GetSpecialValueFor("magic_resistance")
+
+        self:CreateTreant()
+    end
+
+
+    function furion_custom_nature_warrior:CreateTreant()
+        local caster = self:GetCaster()
+
+        local spawn_pos = self.target_pos + RandomVector(250)
+    
+        CreateUnitByNameAsync(
+            "npc_dota_custom_nature_warrior",
+            spawn_pos,
+            true,
+            caster,
+            nil,
+            caster:GetTeamNumber(),
+            function(treant)
+                treant:SetControllableByPlayer(caster:GetPlayerID(), true)
+
+                local treant_hp = caster:GetMaxHealth() * self.health_multiplier
+
+                treant:SetBaseMaxHealth(treant_hp)
+                treant:SetMaxHealth(treant_hp)
+                treant:SetHealth(treant_hp)
+        
+                treant:SetPhysicalArmorBaseValue(caster:GetPhysicalArmorValue() * self.armor_multiplier)
+        
+                treant:SetBaseMagicalResistanceValue(self.magic_resistance)
+            
+                treant:AddNewModifier(treant, self, "modifier_kill", { duration = self.duration })
+            
+                FindClearSpaceForUnit(treant, spawn_pos, true)
+            
+                caster:EmitSound("Hero_Furion.WrathOfNature_Cast")
+            end
+        )
+    end
+end
