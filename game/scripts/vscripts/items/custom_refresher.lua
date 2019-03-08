8074a66df28689abb9731d52fcebad4de7d1f871
pr0crustes <dark.pr0crustes@gmail.com>
2019-03-08 07:43:30 -0300

    Recoded Refresher, attempting to solve stats bug.

--- game/scripts/vscripts/items/custom_refresher.lua
+++ game/scripts/vscripts/items/custom_refresher.lua
@@ -2,13 +2,14 @@ require("lib/my")
 require("lib/refresh")
 
 
+item_custom_refresher = class({})
 
-function refresh(keys)
-    local caster = keys.caster
-    local ability = keys.ability
 
-    local min_cooldown = ability:GetSpecialValueFor("min_cooldown")
-    local max_cooldown = ability:GetSpecialValueFor("max_cooldown")
+function item_custom_refresher:OnSpellStart()
+    local caster = self:GetCaster()
+
+    local min_cooldown = self:GetSpecialValueFor("min_cooldown")
+    local max_cooldown = self:GetSpecialValueFor("max_cooldown")
 
     if not caster:IsTempestDouble() then
 
@@ -16,6 +17,7 @@ function refresh(keys)
 
         local fx = ParticleManager:CreateParticle("particles/items2_fx/refresher.vpcf", PATTACH_ABSORIGIN_FOLLOW, caster)
         ParticleManager:SetParticleControlEnt(fx, 0, caster, PATTACH_POINT_FOLLOW, "attach_hitloc", caster:GetAbsOrigin(), true)
+        ParticleManager:ReleaseParticleIndex(fx)
 
         refresh_abilities(caster, {})
         refresh_items(caster, {})
@@ -28,7 +30,45 @@ function refresh(keys)
 
         local refresher_cd = clamp_value(highest_cd, min_cooldown, max_cooldown)
 
-        ability:StartCooldown(refresher_cd)
+        self:StartCooldown(refresher_cd)
     end
 end
 
+
+function item_custom_refresher:GetIntrinsicModifierName()
+    return "modifier_item_custom_refresher"
+end
+
+
+
+LinkLuaModifier("modifier_item_custom_refresher", "items/custom_refresher.lua", LUA_MODIFIER_MOTION_NONE)
+
+modifier_item_custom_refresher = class({})
+
+
+function modifier_item_custom_refresher:IsHidden()
+    return true
+end
+
+
+function modifier_item_custom_refresher:GetAttributes()
+    return MODIFIER_ATTRIBUTE_MULTIPLE
+end
+
+
+function modifier_item_custom_refresher:DeclareFunctions()
+    return {
+        MODIFIER_PROPERTY_HEALTH_REGEN_CONSTANT,
+        MODIFIER_PROPERTY_MANA_REGEN_CONSTANT,
+    }
+end
+
+
+function modifier_item_custom_refresher:GetModifierConstantHealthRegen()
+    return self:GetAbility():GetSpecialValueFor("bonus_health_regen")
+end
+
+
+function modifier_item_custom_refresher:GetModifierConstantManaRegen()
+    return self:GetAbility():GetSpecialValueFor("bonus_mana_regen")
+end
