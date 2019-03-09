c1302aab6ba2f55cace3e35c9e158b17724282cb
pr0crustes <dark.pr0crustes@gmail.com>
2019-03-09 18:00:47 -0300

    Io Fundamental Heal has a nice particle effect.

--- game/scripts/vscripts/abilities/heroes/wisp_custom_fundamental_heal.lua
+++ game/scripts/vscripts/abilities/heroes/wisp_custom_fundamental_heal.lua
@@ -1,26 +1,61 @@
-require("lib/my")
+require("lib/popup")
 
 
-local previous_hero = nil
+
+wisp_custom_fundamental_heal = class({})
 
 
-function cast_wisp_custom_fundamental_heal(keys)
-    local caster = keys.caster
-    local ability = keys.ability
-    local target = keys.target
+if IsServer() then
+    function wisp_custom_fundamental_heal:OnSpellStart()
+        local caster = self:GetCaster()
+        local target = self:GetCursorTarget()
+    
+        local heal = self:GetSpecialValueFor("base_heal")
+    
+        if target:HasModifier("modifier_wisp_custom_fundamental_heal") then
+            heal = heal * self:GetSpecialValueFor("multiplier")
+            target:RemoveModifierByName("modifier_wisp_custom_fundamental_heal")
+        end
 
-    local base_heal = ability:GetSpecialValueFor("base_heal")
-    local multiplier = ability:GetSpecialValueFor("multiplier")
+        target:AddNewModifier(caster, self, "modifier_wisp_custom_fundamental_heal", {
+            duration = self:GetSpecialValueFor("duration")
+        })
+    
+        caster:EmitSound("Hero_Wisp.Relocate")
+    
+        target:Heal(heal, caster)
 
-    local heal = base_heal
+        create_popup({
+            target = target,
+            value = heal,
+            color = Vector(0, 230, 0),
+            type = "heal"
+        })
+    end
+end
 
-    if target == previous_hero then
-        heal = heal * multiplier
+
+
+LinkLuaModifier("modifier_wisp_custom_fundamental_heal", "abilities/heroes/wisp_custom_fundamental_heal.lua", LUA_MODIFIER_MOTION_NONE)
+
+modifier_wisp_custom_fundamental_heal = class({})
+
+
+if IsServer() then
+    function modifier_wisp_custom_fundamental_heal:OnCreated()
+        local parent = self:GetParent()
+
+        self.particle = ParticleManager:CreateParticle("particles/units/heroes/hero_wisp/wisp_relocate_timer.vpcf", PATTACH_OVERHEAD_FOLLOW, parent)
+        ParticleManager:SetParticleControl(self.particle, 0, parent:GetAbsOrigin())
+        ParticleManager:SetParticleControl(self.particle, 1, Vector(0, 1, 0))
+
+
+        local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_wisp/wisp_relocate_teleport.vpcf", PATTACH_ABSORIGIN_FOLLOW, parent)
+        ParticleManager:ReleaseParticleIndex(particle)
     end
 
-    previous_hero = target
 
-    caster:EmitSound("Hero_Wisp.Relocate")
-
-    target:Heal(heal, ability)
+    function modifier_wisp_custom_fundamental_heal:OnDestroy()
+        ParticleManager:DestroyParticle(self.particle, false)
+    end
 end
