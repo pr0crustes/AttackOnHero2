fbb577597f4b6bd1446f8dd5f3c02230ef637a42
pr0crustes <dark.pr0crustes@gmail.com>
2019-03-16 06:33:30 -0300

    Centaur Return: Now has a "0.3" cooldown.

--- game/scripts/vscripts/abilities/heroes/centaur_custom_return.lua
+++ game/scripts/vscripts/abilities/heroes/centaur_custom_return.lua
@@ -31,7 +31,7 @@ if IsServer() then
         local unit = keys.unit
         local attacker = keys.attacker
 
-        if unit == self:GetParent() and unit ~= attacker then
+        if unit == self:GetParent() and unit ~= attacker and not unit:HasModifier("modifier_centaur_custom_return_cooldown") then
             local ability = self:GetAbility()
 
             local base_damage = ability:GetSpecialValueFor("base_damage")
@@ -52,7 +52,21 @@ if IsServer() then
             ParticleManager:SetParticleControlEnt(particle, 0, unit, PATTACH_POINT_FOLLOW, "attach_hitloc", unit:GetAbsOrigin(), true)
             ParticleManager:SetParticleControlEnt(particle, 1, attacker, PATTACH_POINT_FOLLOW, "attach_hitloc", attacker:GetAbsOrigin(), true)
             ParticleManager:ReleaseParticleIndex(particle)
+
+            unit:AddNewModifier(caster, ability, "modifier_centaur_custom_return_cooldown", {
+                duration = ability:GetSpecialValueFor("cooldown")
+            })
         end
     end
 end
 
+
+
+LinkLuaModifier("modifier_centaur_custom_return_cooldown", "abilities/heroes/centaur_custom_return.lua", LUA_MODIFIER_MOTION_NONE)
+
+modifier_centaur_custom_return_cooldown = class({})
+
+
+function modifier_centaur_custom_return_cooldown:IsHidden()
+    return true
+end
