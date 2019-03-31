ae78434189ac68b63c88d6bbad28c58de6a45051
pr0crustes <dark.pr0crustes@gmail.com>
2019-03-31 14:39:33 -0300

    Reworked Riki's Q.

--- game/scripts/vscripts/abilities/heroes/riki_custom_trick_hit.lua
+++ game/scripts/vscripts/abilities/heroes/riki_custom_trick_hit.lua
@@ -2,66 +2,85 @@ require("lib/my")
 require("lib/timers")
 
 
-LinkLuaModifier("modifier_riki_custom_trick_hit", "abilities/heroes/riki_custom_trick_hit.lua", LUA_MODIFIER_MOTION_NONE)
-
-
-
 riki_custom_trick_hit = class({})
 
 
 function riki_custom_trick_hit:GetIntrinsicModifierName()
-    return "modifier_riki_custom_trick_hit"
+    return "modifier_riki_custom_trick_hit_autocast"
+end
+
+
+function riki_custom_trick_hit:OnSpellStart()
+    local caster = self:GetCaster()
+
+    if caster:HasModifier("modifier_riki_custom_trick_hit") then
+        caster:RemoveModifierByName("modifier_riki_custom_trick_hit")
+    end
+
+    local modifier = caster:AddNewModifier(caster, self, "modifier_riki_custom_trick_hit", {})
+
+    local bonus_agi = caster:GetAgility() * self:GetSpecialValueFor("bonus_agi_pct") * 0.01
+    modifier:SetStackCount(bonus_agi)
 end
 
 
 
-modifier_riki_custom_trick_hit = class({})
+LinkLuaModifier("modifier_riki_custom_trick_hit_autocast", "abilities/heroes/riki_custom_trick_hit.lua", LUA_MODIFIER_MOTION_NONE)
+
+modifier_riki_custom_trick_hit_autocast = class({})
 
 
-function modifier_riki_custom_trick_hit:IsHidden()
+function modifier_riki_custom_trick_hit_autocast:IsHidden()
     return true
 end
 
 
-function modifier_riki_custom_trick_hit:IsPurgable()
-    return false
+if IsServer() then
+    function modifier_riki_custom_trick_hit_autocast:OnCreated(keys)
+        self:StartIntervalThink(0.1)
+    end
+
+
+    function modifier_riki_custom_trick_hit_autocast:OnIntervalThink()
+		local ability = self:GetAbility()
+        local parent = self:GetParent()
+
+        if parent:IsAlive()
+            and ability:GetAutoCastState()
+            and not parent:HasModifier("modifier_riki_custom_trick_hit")
+            and parent:GetMana() >= ability:GetManaCost(ability:GetLevel() - 1)
+            and not parent:IsChanneling()
+            and not parent:IsInvisible()
+            and not (parent:GetCurrentActiveAbility() and parent:GetCurrentActiveAbility():IsInAbilityPhase()) then
+
+            parent:CastAbilityNoTarget(ability, parent:GetPlayerID())
+		end
+    end
 end
 
 
+
+
+LinkLuaModifier("modifier_riki_custom_trick_hit", "abilities/heroes/riki_custom_trick_hit.lua", LUA_MODIFIER_MOTION_NONE)
+
+modifier_riki_custom_trick_hit = class({})
+
+
 function modifier_riki_custom_trick_hit:DeclareFunctions()
-    local funcs = {
-		MODIFIER_EVENT_ON_ATTACK_LANDED,
-	}
-	return funcs
+    return {
+        MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
+        MODIFIER_EVENT_ON_ATTACK_LANDED,
+    }
+end
+
+
+function modifier_riki_custom_trick_hit:GetModifierBonusStats_Agility()
+    return self:GetStackCount()
 end
 
 
 function modifier_riki_custom_trick_hit:OnAttackLanded(keys)
-    local ability = self:GetAbility()
-    local attacker = keys.attacker
-    local attack_damage = keys.original_damage
-    local target = keys.target
-
-    if attacker ~= self:GetParent() then 
-        return 
+    if keys.attacker == self:GetParent() then 
+        self:Destroy()
     end
-
-    local magic_percentage = ability:GetSpecialValueFor("percentage")
-
-    local damage = attack_damage * magic_percentage * 0.01
-
-    Timers:CreateTimer(
-        0.1,
-        function()
-            ApplyDamage({
-                victim = target,
-                attacker = attacker,
-                ability = ability,
-                damage_type = DAMAGE_TYPE_MAGICAL,
-                damage = damage,
-                damage_flags = DOTA_DAMAGE_FLAG_NO_SPELL_AMPLIFICATION,
-            })
-            return nil
-        end
-    )
 end
